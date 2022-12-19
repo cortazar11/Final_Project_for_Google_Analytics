d170e8acffe3b6b499ba4b3d9554fe32f79a24e7
cortazar11 <miguel.martinez11@gmail.com>
2022-12-19 12:27:44 +0100

    Update Rmd

--- proyecto.R
+++ proyecto.R
@@ -1,1 +1,21 @@
 install.packages("tidyverse")
+
+library(tidyverse)
+library(readr)
+library(dplyr)
+
+
+df <- read.csv('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/YEARS/202004-divvy-tripdata.csv')
+str(df)
+list_files <-list.files(path='/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/YEARS') 
+list_files
+months<-substr(list_files,0,6)
+months
+result <- lapply(list_files, function(x){
+      df<-read.csv(paste0('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/YEARS/',x))
+      table(df$member_casual)
+      
+})
+names(result) <- months
+class(result)
+result$`202005`
