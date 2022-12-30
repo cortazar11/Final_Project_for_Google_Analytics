34f376900d8e9fa288cbb7fd330a586848d5b8c4
Miguel Martinez <miguel.martinez11@gmail.com>
2022-12-30 12:33:27 +0100

    Update proyecto.R

--- proyecto.R
+++ proyecto.R
@@ -1,12 +1,21 @@
+install.packages("tidyverse")
+install.packages("generics")
 library(tidyverse)
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(data.table)
 library(lubridate)
+library(generics)
 
 my_data1 <- read_csv('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv')
-class(my_data1$starttime)
+my_data1$usertype[my_data1$usertype=="Customer"] <- "casual"
+my_data1$usertype[my_data1$usertype=="Subscriber"] <- "member"
+write.csv(my_data1,'/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv',row.names =FALSE)
+unique(my_data1$usertype)
+my_data1$usertype <- as.factor(my_data1$usertype)
+write.csv(my_data1,'/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv',row.names=FALSE)
+
 my_data2 <- read_csv('CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2014_A_Q1Q2.csv')
 my_data2$starttime[1]
 
@@ -30,6 +39,7 @@ raw.data <- raw.file.paths %>%
 
 # CLEANING FILES
 
+# Renaming columns
 lapply(list_files_final[23:24],
           function(x) {
                 my_data <-read_csv(x)
@@ -48,15 +58,19 @@ lapply(list_files_final[22],
         }
        )
 
+# Converting to date type
+
 as.POSIXct(my_data2$starttime, tz = "", format="%m/%d/%Y %H:%M")
 
 lapply(list_files_final[2:16],
                  function(x){
                    my_data <- read_csv(x)
-                   as.POSIXct(my_data$starttime, tz = "", format="%m/%d/%Y %H:%M")
-                   as.POSIXct(my_data$stoptime, tz = "", format="%m/%d/%Y %H:%M")
+                   my_data$starttime <-as.POSIXct(my_data$starttime, tz = "", format="%m/%d/%Y %H:%M")
+                   my_data$stoptime <- as.POSIXct(my_data$stoptime, tz = "", format="%m/%d/%Y %H:%M")
+                   write.csv(my_data,x,row.names=FALSE)
                  })
 
+# Class of starttime and stoptime
 result <- lapply(list_files_final,
        function(x){
          my_data <- read_csv(x)
@@ -64,6 +78,8 @@ result <- lapply(list_files_final,
          class(my_data$stoptime)
        })
 result
+
+
 
 list_files_trips <-list.files(path='/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/TRIPS/')
 list_files_trips
@@ -76,29 +92,13 @@ names_trips
 names_years <- names(read.csv(paste0('/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/YEARS/',list_files_years[1])))
 names_years
 
-# lapply(list_files, 
-#       function(x) 
-#         write.csv(read.csv(paste0('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/YEARS/',x))[-c(1)],paste0("CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/YEARS/",x),row.names=FALSE)
-#       )
 
-lapply(list_files_years, 
-       function(x) {
-         df<- read.csv(paste0('/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/YEARS/',x))
-         names(df) <- c("trip_id","rideable_type" ,"starttime","stoptime" ,
-                        "from_station_name","from_station_id" ,"to_station_name" ,
-                        "to_station_id","start_lat" ,"start_lng" ,"end_lat" ,"end_lng",
-                        "usertype" )  
-         
-         write.csv(df,paste0('/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/YEARS/',x),row.names=FALSE)
-       }
-)
-
-# Data frame from list of files
+# Data fr from list of files
 
 list_files_final <- list.files(path='/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2',full.names = TRUE)
 list_files_final
 
-starttime_type <- lapply(list_files_final,
+starttime_type <- lapply(list_files_final[1],
                          function(x){
                            spec(read_csv(x))
                            
@@ -106,6 +106,17 @@ starttime_type <- lapply(list_files_final,
 )
 
 starttime_type
+
+# Converting usertype from character to factor and doing a chartbar.
+lapply(list_files_final,
+       function(x){
+         my_data <- read_csv(x)
+         my_data$usertype[my_data$usertype=="Customer"] <- "casual"
+         my_data$usertype[my_data$usertype=="Subscriber"] <- "member"
+         write.csv(my_data,x,row.names=FALSE)
+       })
+
+
 
 df <- list.files(path = "/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2", pattern = "*.csv") %>% 
   map_df(~fread(.))
