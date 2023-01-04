40e8b3796f1c3b18b82d57157403a4357932c266
Miguel Martinez <miguel.martinez11@gmail.com>
2023-01-04 12:42:38 +0100

    Update proyecto.R

--- proyecto.R
+++ proyecto.R
@@ -2,6 +2,8 @@ install.packages("tidyverse")
 install.packages("generics")
 install.packages("stringi")
 install.packages("descriptr")
+install.packages("psych")
+install.packages("swirl")
 library(tidyverse)
 library(readr)
 library(dplyr)
@@ -11,6 +13,8 @@ library(lubridate)
 library(generics)
 library(stringi)
 library(descriptr)
+library(psych)
+library(swirl)
 
 list_files <- list.files("/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA_12MONTHS", full.names= TRUE)
 list_files
@@ -27,6 +31,8 @@ result <- lapply(list_files,
                                                       replacement=c('', '', '' ),
                                                       vectorize=FALSE)
                    df$diff <- df$ended_at - df$started_at
+                   df$diff_double <- as.double(df$diff, units="auto")
+                   df$diff_format <- hms::hms(seconds_to_period(df$diff))
                    df
                  }
 )
@@ -36,20 +42,22 @@ class(result)
 df <- do.call(rbind,result)
 
 
-# Converting drtn to time
-df$diff_format <- hms::hms(seconds_to_period(df$diff))
+
 
 colnames(df)
+ds_screener(df)
 glimpse(df)
-diff_double <-as.double(df$diff)
-range(diff_double, na.rm = FALSE)
-sum(sign(diff_double)==0)
-
+range(df$diff_double, na.rm = FALSE)
+sum(sign(df$diff_double)==0)
+sum(sign(df$diff_double)==-1)
 mean(df)
 diff_num[1:10]
-stats_time <- summary(diff_double)
-stats_time
-# Grouping by month
+
+# Subsetting the data frame.
+df <- subset(df, !sign(df$diff_double)==-1)
+df <- subset(df, !sign(df$diff_double)==0)
+result
+# Grouping by month and member_casual
 df$member_casual <- as.factor(df$member_casual)
 by_month_member <- df %>%
               group_by(month) %>%
@@ -60,141 +68,26 @@ p <- ggplot(by_month, aes(fill=member_casual,y=n,x=month )) +
   geom_bar(position="dodge",stat="identity")
 p
 
-# Grouping by month the times use
-mean_time <- 
-by_month_time <- df %>%
-                      group_by(month) %>%
-                      count(diff_format)
-by_month_time
+# Grouping by month the time is used
+typeof(df$diff_double)
+class(df$diff_double)
+e
+typeof(df$diff)
+sapply(df$diff_double, mean, na.rm=TRUE)
 
+df <- subset(df, !sign(df$diff_double)==-1)
+df <- subset(df, !sign(df$diff_double)==0)
+count(df)
+by_month <- df %>%
+              group_by(month) %>%
+              #summarise_at(vars(diff_double), list(name = mean))
+                      
+by_month
+
+summary(df$diff_double)
+result <- sapply(df$diff_double, mean, na.rm=TRUE)
+stats_by_month <- describeBy(df$diff_double, df$month, mat = TRUE) 
+stats_by_month
 p <- ggplot(by_month_time, aes(fill=member_casual,y=n,x=month )) +
   geom_bar(position="dodge",stat="identity")
 p
-
-
-
-# Data about rideable_type
-unique(df$rideable_type)
-
-# df <- list.files(path = "/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA_LAST_12_MONTH", pattern = "*.csv",full.names = TRUE) %>% 
-#   map_df(~fread(.))
-# glimpse(df)
-
-
-
-
-
-##############################################################################################################################3
-my_data1 <- read_csv('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv')
-my_data1$usertype[my_data1$usertype=="Customer"] <- "casual"
-my_data1$usertype[my_data1$usertype=="Subscriber"] <- "member"
-write.csv(my_data1,'/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv',row.names =FALSE)
-unique(my_data1$usertype)
-my_data1$usertype <- as.factor(my_data1$usertype)
-write.csv(my_data1,'/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2013.csv',row.names=FALSE)
-
-my_data2 <- read_csv('CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2/Divvy_Trips_2014_A_Q1Q2.csv')
-my_data2$starttime[1]
-
-as.POSIXct(my_data2$starttime, tz = "", format="%m/%d/%Y %H:%M")
-
-#list_files <-list.files(path='/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/TRIPS')
-#list_files
-# MAKING A DATAFRAME OF ALL THE FILES
-
-raw.files <- tibble(filename = list.files('/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/TRIPS'))
-raw.files
-
-raw.file.paths <- raw.files  %>%
-  mutate(filepath = paste0("/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA/TRIPS", filename))
-raw.file.paths
-
-raw.data <- raw.file.paths %>%
-  rowwise() %>%
-  do(., read_csv(file=.$filepath))
-
-
-# CLEANING FILES
-
-# Renaming columns
-lapply(list_files_final[23:24],
-          function(x) {
-                my_data <-read_csv(x)
-                names(my_data)[names(my_data) == "start_time"] <- "starttime"
-                names(my_data)[names(my_data) == "end_time"] <- "stoptime"
-                write.csv(my_data,x,row.names=FALSE)
-          }
-       )
-
-lapply(list_files_final[22],
-        function(x) {
-          my_data <- read_csv(x)
-          names(my_data) <- c("trip_id","starttime", "stoptime", "bikeid", "tripduration", "from_station_id", "from_station_name",
-                              "to_station_id", "to_station_name", "usertype", "gender", "birthyear")
-          write.csv(my_data,x,row.names=FALSE)
-        }
-       )
-
-# Converting to date type
-
-as.POSIXct(my_data2$starttime, tz = "", format="%m/%d/%Y %H:%M")
-
-lapply(list_files_final[2:16],
-                 function(x){
-                   my_data <- read_csv(x)
-                   my_data$starttime <-as.POSIXct(my_data$starttime, tz = "", format="%m/%d/%Y %H:%M")
-                   my_data$stoptime <- as.POSIXct(my_data$stoptime, tz = "", format="%m/%d/%Y %H:%M")
-                   write.csv(my_data,x,row.names=FALSE)
-                 })
-
-# Class of starttime and stoptime
-result <- lapply(list_files_final,
-       function(x){
-         my_data <- read_csv(x)
-         class(my_data$starttime)
-         class(my_data$stoptime)
-       })
-result
-
-
-
-list_files_trips <-list.files(path='/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/TRIPS/')
-list_files_trips
-list_files_years <-list.files(path='/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/YEARS/')
-list_files_years
-
-names_trips <- names(read.csv(paste0('/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/TRIPS/',list_files_trips[1])))
-names_trips
-
-names_years <- names(read.csv(paste0('/home/miguel/CURSO_ANALISIS_DATOS/Caso_Práctico/DATA/YEARS/',list_files_years[1])))
-names_years
-
-
-# Data fr from list of files
-
-list_files_final <- list.files(path='/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2',full.names = TRUE)
-list_files_final
-
-starttime_type <- lapply(list_files_final[1],
-                         function(x){
-                           spec(read_csv(x))
-                           
-                         }
-)
-
-starttime_type
-
-# Converting usertype from character to factor and doing a chartbar.
-lapply(list_files_final,
-       function(x){
-         my_data <- read_csv(x)
-         my_data$usertype[my_data$usertype=="Customer"] <- "casual"
-         my_data$usertype[my_data$usertype=="Subscriber"] <- "member"
-         write.csv(my_data,x,row.names=FALSE)
-       })
-
-
-
-df <- list.files(path = "/home/miguel/CURSO_DATA_ANALYSIS/Caso_Práctico/DATA2", pattern = "*.csv") %>% 
-  map_df(~fread(.))
-df
